060487589819b2bcf6635a5bfbf50b0fbbacc7ef
EhssanBx0 <ehssanbaghban@outlook.com>
2025-09-02 11:43:01 +0100

    create Book constructor and addBook... func to create and add books to library

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, date) {
+    // the constructor...
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.date = date;
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, date) {
+    // take params, create a book then store it in the array
+    let newBook = new Book(title, author, date);
+    myLibrary.push(newBook);
+    console.log("New Book added to Library: ")
+    console.dir(newBook);
 }
+
+addBookToLibrary("Dune", "Frank Herbert", "1967");
